dd0083a2f0750f634b5d54617ed5e4831927a655
abhinavanand-fed <abhinavanand080@gmail.com>
2023-03-31 13:47:44 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiEndpoint = 'https://randomuser.me/api/?results=500';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loadMore = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      axios.get(apiEndpoint).then((response) => {
+        setUserList([...userList, ...response.data.results]);
+        setIsLoading(false);
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    // check if user is logged in
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    setIsLoggedIn(isLoggedIn);
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    if (username === 'foo' && password === 'bar') {
+      localStorage.setItem('isLoggedIn', 'true');
+      setIsLoggedIn(true);
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.setItem('isLoggedIn', 'false');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+          <ul style={{ listStyle: 'none' }}>
+            {userList.map((user, index) => (
+              <li key={index}>
+                <img src={user.picture.thumbnail} alt={user.name.first} />
+                <span>{user.name.first} {user.name.last}</span>
+              </li>
+            ))}
+            {isLoading && <li>Loading...</li>}
+          </ul>
+          {!isLoading && (
+            <button onClick={loadMore}>
+              Load more
+            </button>
+          )}
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={handleLogin}>
+            <input type="text" placeholder="Username" name="username" />
+            <input type="password" placeholder="Password" name="password" />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
